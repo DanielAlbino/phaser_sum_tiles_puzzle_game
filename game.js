fee3b2030bff7df2eaf75d183a75b22cf0a803bc
Daniel Albino <daniel18albino@gmail.com>
2020-08-31 15:35:50 +0100

    created tile animation

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ var gameOptions = {
     rows: 4,
     cols: 4,
   },
+  tweenSpeed: 1000,
 };
 window.onload = () => {
   var gameConfig = {
@@ -48,6 +49,7 @@ class playGame extends Phaser.Scene {
   }
 
   create() {
+    this.canMove = false;
     this.boardArray = [];
     for (var i = 0; i < 4; i++) {
       this.boardArray[i] = [];
@@ -58,10 +60,12 @@ class playGame extends Phaser.Scene {
         tile.visible = false;
         this.boardArray[i][j] = {
           tileValue: 0,
-          tielSprite: true,
+          tileSprite: tile,
         };
       }
     }
+    this.addTile();
+    this.addTile();
   }
 
   getTilePosition(row, col) {
@@ -70,6 +74,37 @@ class playGame extends Phaser.Scene {
     var posY =
       gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
     return new Phaser.Geom.Point(posX, posY);
+  }
+
+  addTile() {
+    var emptyTiles = [];
+    for (var i = 0; i < gameOptions.boardSize.rows; i++) {
+      for (var j = 0; j < gameOptions.boardSize.cols; j++) {
+        if (this.boardArray[i][j].tileValue == 0) {
+          emptyTiles.push({
+            row: i,
+            col: j,
+          });
+        }
+      }
+    }
+    if (emptyTiles.length > 0) {
+      var chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles);
+      this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1;
+      this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
+      this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0);
+      this.boardArray[chosenTile.row][chosenTile.col].tileSprite.alpha = 0;
+      this.tweens.add({
+        targets: [this.boardArray[chosenTile.row][chosenTile.col].tileSprite],
+        alpha: 1,
+        duration: gameOptions.tweenSpeed,
+        callbackScope: this,
+        onComplete: function () {
+          console.log("tween completed");
+          this.canMove = true;
+        },
+      });
+    }
   }
 } //class
 
